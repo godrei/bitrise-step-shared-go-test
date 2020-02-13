c1dc6114d3aed6bfc998e27f92b62b1bed204618
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2020-02-13 13:05:41 +0100

    step fix

--- step.sh
+++ step.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -ex
 
-bitrise run go-test -c ./shared-bitrise.yml
+bitrise run go-test -c "$THIS_SCRIPT_DIR/shared-bitrise.yml"
